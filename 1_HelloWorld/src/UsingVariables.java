8bb93518762fdd873307c85831a08d330c494114
Martina Alinda <martinaalinda1@gmail.com>
2020-05-13 21:15:13 +0300

    Initial Commit

---
+++ 1_HelloWorld/src/UsingVariables.java
@@ -0,0 +1,50 @@
+
+public class UsingVariables {
+	
+	public static void main(String[] args) {
+		
+		// Variables are like boxes that can hold things
+		// There are 8 different primitive types of Variables
+		// An int is a 32 bit value
+		// Short is a 16 bit value
+		// Long is a 64 bit value for really big numbers
+		// Double
+		// Float
+		// Char which can hold a character (in single quotes)
+		// Boolean holds true or false
+		// Byte which holds 8 bits of data
+		
+		
+		// Declared the Variable
+		int myNumber;
+		
+		// Initializing the variable
+		myNumber = 88;
+		
+		// We can declare and initialize together
+		// int myNumber = 88;
+		
+		short myShort = 847;
+		long myLong = 9797;
+		double myDouble = 7.3546;
+		
+		// For the float add an "f" at the end to convert from double to float
+		float myFloat = 124.54f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+	}
+
+}
